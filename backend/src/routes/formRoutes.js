2e4beee1cf7a4748919db39f21628de197f096d7
Amit Raj <amitraj4381@gmail.com>
2025-07-10 20:50:44 +0530

    added tracking

--- backend/src/routes/formRoutes.js
+++ backend/src/routes/formRoutes.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const router = express.Router();
 
 // Email transporter setup
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
